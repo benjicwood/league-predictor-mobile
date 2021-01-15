9b4d4b60773cd211a6c9b9c2831534ff74fe26f4
benjicwood <bencharlwood@gmail.com>
2021-01-15 16:44:37 +0000

    league table

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,20 +1,56 @@
 import * as React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, View, Text, Image } from 'react-native';
 
 import LEAGUE from '../data/league';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+// import { Text, View } from '../components/Themed';
+
+const header = [
+  'Logo',
+  'Team',
+  'Pld',
+  'W',
+  'D',
+  'L',
+  'GD',
+  'P',
+];
+
+// // Indices (columns) to include on a small screen
+// export const smallScreenIndices = [0, 1, 5];
+
+// // Indices to include on a medium screen
+// export const mediumScreenIndices = [0, 1, 2, 4, 5];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>League</Text>
+      {/* <Text style={styles.title}>League</Text> */}
       {/* <FlatList data={LEAGUE} KeyExtractor={item => item.team}></FlatList> */}
-      <FlatList data={LEAGUE} keyExtractor={item => item.team} renderItem={({item}) => (
-                <View>
-                    <Text>{item.team}</Text>
-                    <Text>{item.points}</Text>
+
+      <View style={styles.header}>
+                    <Text style={{width: '10%'}}></Text>
+                    <Text style={{width: '42%'}}>Team</Text>
+                    <Text style={{width: '8%'}}>Pld</Text>
+                    <Text style={{width: '8%'}}>W</Text>
+                    <Text style={{width: '8%'}}>D</Text>
+                    <Text style={{width: '8%'}}>L</Text>
+                    <Text style={{width: '8%'}}>GD</Text>
+                    <Text style={{width: '10%'}}>P</Text>
+                </View>
+      <FlatList style={styles.leagueTable} data={LEAGUE} keyExtractor={item => item.team} renderItem={({item}) => (
+                <View style={styles.team}>
+                    <Text style={{width: '10%'}}>
+                      <Image style={styles.logo} source={require(`./club-logos/${item.icon}.png`)}></Image>
+                    </Text>
+                    <Text style={{width: '42%', marginTop: 3}}>{item.team}</Text>
+                    <Text style={{width: '8%', marginTop: 3}}>{item.played}</Text>
+                    <Text style={{width: '8%', marginTop: 3}}>{item.won}</Text>
+                    <Text style={{width: '8%', marginTop: 3}}>{item.drawn}</Text>
+                    <Text style={{width: '8%', marginTop: 3}}>{item.lost}</Text>
+                    <Text style={{width: '8%', marginTop: 3}}>{item.gd}</Text>
+                    <Text style={{width: '10%', marginTop: 3}}>{item.points}</Text>
                 </View>
             )} />
     </View>
@@ -36,4 +72,27 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  leagueTable: {
+    width: '100%',
+  },
+  team: {
+    display: 'flex',
+    flexDirection: 'row',
+    height: 30,
+  },
+  logo: {
+    width: 25,
+    height: 25,
+    resizeMode: 'contain',
+  },
+  header: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  headerItem:{
+    height: 30,
+  }
 });
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, View, Text, TextInput } from 'react-native';
 
 import FIXTURES from '../data/fixtures';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+// import { Text, View } from '../components/Themed';
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Fixtures</Text>
       <FlatList data={FIXTURES[0]} keyExtractor={item => item.home} renderItem={({item}) => (
-                <View>
+                <View style={styles.fixture}>
                     <Text>{item.home}</Text>
+                    <TextInput style={styles.textInput}></TextInput>
+                    <TextInput style={styles.textInput}></TextInput>
                     <Text>{item.away}</Text>
                 </View>
             )} />
@@ -20,7 +22,18 @@ export default function TabTwoScreen() {
   );
 }
 
+// { home: 'Sheffield United', homeScore: null, awayScore: null, away: 'Newcastle United' },
+
 const styles = StyleSheet.create({
+  textInput: {
+    backgroundColor: 'whitesmoke',
+    width: 30,
+    height: 30,
+    border: '1px solid black',
+  },
+  fixture: {
+    flexDirection: 'row',
+  },
   container: {
     flex: 1,
     alignItems: 'center',
